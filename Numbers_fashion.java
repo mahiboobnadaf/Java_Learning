831b9799fede26aa8488d5f91824ac975207962c
mahiboobnadaf <rasool97260@gmail.com>
2024-02-05 00:13:56 +0530

    dual for loop leaning

---
+++ Numbers_fashion.java
@@ -0,0 +1,14 @@
+public class Numbers_fashion {
+    public static void main(String[] args) {
+        System.out.println("Screen Display");
+        for(int i=1;i<=9;i++){
+            System.out.println("i:" +i);
+            for(int j=1;j<=i;j++){
+                // System.out.println("\t");
+                System.out.println("J:"+j);
+            }
+            System.out.println("\n");
+        }
+        System.out.println("Screen Displayed");
+    }
+}
