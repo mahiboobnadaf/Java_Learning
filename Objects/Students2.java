2794f7578ccf6197eb114cd53faa79c34c91476b
mahiboobnadaf <rasool97260@gmail.com>
2024-02-05 17:10:36 +0530

    object creation from outer class

---
+++ Objects/Students2.java
@@ -0,0 +1,19 @@
+package Objects;
+
+class Records {
+    int id;
+    String name;
+
+}
+
+public class Students2 {
+
+    public static void main(String[] args) {
+        Records s1 = new Records();
+
+        s1.id=14;
+        s1.name="mahiboob nadaf";
+
+        System.out.println(s1.id +" "+s1.name );
+    }
+}
