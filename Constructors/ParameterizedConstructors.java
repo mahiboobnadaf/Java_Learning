1c7533fb3f8b32f4720f08129e50dd9272359928
mahiboobnadaf <rasool97260@gmail.com>
2024-02-06 14:04:51 +0530

    detailed constructr example

--- Constructors/ParameterizedConstructors.java
+++ Constructors/ParameterizedConstructors.java
@@ -1,11 +1,23 @@
 package Constructors;
 
 public class ParameterizedConstructors {
-    ParameterizedConstructors(int rno,String name){
-        System.out.println("name: "+name + " id: "+rno);
+
+    int id;
+    String name;
+    ParameterizedConstructors(int rno,String naam){
+        id=rno;
+        name=naam;
+        System.out.println("name: "+naam + " id: "+rno +" From object 1 "); //print 2times for 2 objects bcoz we are passing params to constructors and prnting it below
+    }
+
+    void display(){
+        System.out.println("name: "+name + " id: "+id + " From Object 2 ");
     }
 
     public static void main(String[] args) {
         ParameterizedConstructors s1 = new ParameterizedConstructors(11, "Mahiboob");  //passing values to parameterized constructor
+        ParameterizedConstructors s2 = new ParameterizedConstructors(22, "Nadaf");  //passing values to parameterized constructor
+        
+        s2.display();
     }
 }
