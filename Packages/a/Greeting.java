a8af509547287b5d913d1e1d628d10d5f458c8c9
mahiboobnadaf <rasool97260@gmail.com>
2024-02-08 01:08:47 +0530

    packages imports

--- Packages/a/Greeting.java
+++ Packages/a/Greeting.java
@@ -1,14 +1,15 @@
 package Packages.a;
 
 import static Packages.b.Message.message;       //importing the package from where i need to call the method
-import static Packages.a.Sms.brother;
+// import static Packages.a.Sms.brother;        //no need to import if the file is in same folder
 
 
 public class Greeting {
     public static void main(String[] args) {
         System.out.println("Hello Welcome to Greeting.java");
-        brother();                              // calling a method from same folder but other file
+        Sms.brother();                              // calling a method from same folder but other file
         message();                              // Calling the method of other file
     }
 
 }
+ 
