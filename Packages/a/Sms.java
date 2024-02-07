f4bfd19318e04f83c91fa78a678d829a3955127d
mahiboobnadaf <rasool97260@gmail.com>
2024-02-07 19:08:54 +0530

    example of importing packages

---
+++ Packages/a/Sms.java
@@ -0,0 +1,10 @@
+package Packages.a;
+
+public class Sms {
+    public static void main(String[] args) {
+        
+    }
+    public static void brother(){
+        System.out.println("Hi buddy i am from same folder where my brother greeting lives.");
+    }
+}
---
+++ Packages/b/Message.java
@@ -0,0 +1,13 @@
+package Packages.b;
+
+public class Message {
+    public static void main(String[] args) {
+
+       
+    }
+    public static void message() {
+        System.out.println("hey! i am from Message.java");
+
+    }
+   
+}
